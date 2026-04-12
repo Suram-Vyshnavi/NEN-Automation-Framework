c6294aaf79ecd9ecfef398d20add2d5bafbb0ccb
Suram vyshmavi <vyshnavi.suram@wadhwani.org>
2026-04-12 20:03:26 +0530

    chore: keep reports-only branch layout

--- locators/Incubator_locators/partner_profile_locators.py
+++
@@ -1,6 +0,0 @@
-class partner_profile_locators:
-    MY_PROFILE= "//div[@class='ml-3 profile_container']"
-    PARTNER_PROFILE_TAB = "//h1[text()='Partner Profile']"
-    PARTNER_PROFILE_HEADING = "//h4[text()='Profile information']"
-    NAME_INPUT = "//input[@id='name']"
-    CHANGE_LOGO = "//span[text()='Change Logo']"
--- locators/student_locators/events_locators.py
+++
@@ -1,7 +0,0 @@
-class Eventlocators:
-    EVENTS_HEADING="(//div[text()='Events'])[1]"
-    UPCOMING_EVENTS="//h2[text()='Upcoming Events']"
-    REGISTERED_EVENTS="//h1[text()='Registered Events']"
-    VIEW_DETAILS="(//h2[text()='View Details'])[1]"
-    EXPLORE_EVENTS="//h1[text()='Explore Events']"
-    BACK_BUTTON="//button//img"
